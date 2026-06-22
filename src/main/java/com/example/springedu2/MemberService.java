fdf64ef918891817d575299ac217d3f16a51871a
SinSeungMin7 <min85112000@gmail.com>
2026-06-22 14:03:54 +0900

    Member Register2

--- src/main/java/com/example/springedu2/MemberService.java
+++ src/main/java/com/example/springedu2/MemberService.java
@@ -1,7 +1,10 @@
 package com.example.springedu2;
 
+import com.example.springedu2.dto.MemberCreateForm;
 import com.example.springedu2.entity.Member;
+import com.example.springedu2.entity.Role;
 import com.example.springedu2.repository.MemberRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,18 +22,55 @@ public class MemberService implements UserDetailsService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
+
+    // 로그인을 위해 db 에서 회원정보를 조회해서 UserDetails를 생성
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         // 회원정보 db 에서 회원이름으로 조회
-        Member member = memberRepository.findByUsername(username )
+        Member member = memberRepository.findByUsername( username )
                 .orElseThrow( () -> new UsernameNotFoundException(
-                        "존재하지 않는 사용자 입니다." ) );
-        // 조합한 결과를 Member -> UserDetails 으로 변환
-        return User.builder()
-                .username(member.getUsername())      // 사용자 Id
-                .password(member.getPassword())      // 사용자 비밀번호
-                .disabled(!member.isEnabled())        // 계정 사용 가능 정보
-                .roles(member.getRole().toString())  // 사용자 권한 " ADMIN" -> ROLE_ADMIN 권한을
+                        "존재하지 않는 사용자입니다") );
+
+        // 조회한 결과를 Member -> UserDetails 으로 변환
+        UserDetails user = User.builder()
+                .username(member.getUsername())        // 사용자 id
+                .password(member.getPassword())        // 사용자 비밀번호
+                .disabled(!member.isEnabled())          // 계정 사용 가능
+                .roles(member.getRole().toString())    // 사용자 권한 "ADMIN" -> ROLE_ADMIN 권한
                 .build();
+        return  user;
     }
+
+    //-------------------------------------------------------
+
+    // 일반 유저 회원가입
+    public Member register(MemberCreateForm memberForm) {
+        memberForm.setRole( Role.USER.name() );
+        return create(memberForm);
+    }
+
+    // 회원가입
+    @Transactional
+    private Member create(MemberCreateForm memberForm) {
+        // 기존회원인지 조회
+
+        Member member = new Member();
+        member.setUsername( memberForm.getUsername() );
+        member.setPassword( passwordEncoder.encode( memberForm.getPassword() ) );
+        member.setName( memberForm.getName() );
+        member.setEmail( memberForm.getEmail() );
+        member.setRole( parseRole( member.getRole() ) );
+        member.setEnabled( true );
+        return  memberRepository.save( member );
+    }
+
+    // 권한 문자열 변환 "ADMIN", "USER" -> Role.ADMIN
+    private Role parseRole(String role) {
+        if( role == null || role.isBlank() ) {
+            return Role.USER;
+        }
+        return Role.valueOf( role.toUpperCase() );
+    }
+
+
 }
